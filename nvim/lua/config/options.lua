2578de9ffab2e329725e77225f280726a48189b0
neokail <qo9@hotmail.fr>
2025-08-23 13:47:03 +0300

    add some options to nvim/config/options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,29 +4,33 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+opt.scrolloff = 10 -- Keep 10 lines above/below cursor
+opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+opt.cmdheight = 1 -- Command line height
+opt.spelllang = { "en", "ar" } -- Set language for spellchecking
+
+opt.wrap = true -- Wrap the content of the screen
+opt.linebreak = true
+opt.breakindent = true
+opt.cursorline = true
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
 opt.smartindent = true
-
-opt.wrap = true
-opt.linebreak = true
-opt.breakindent = true
+opt.autoindent = true -- copy indent from current line when starting new one
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
-opt.cursorline = true
 
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.showmatch = true -- Hightlight matching brackets
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
